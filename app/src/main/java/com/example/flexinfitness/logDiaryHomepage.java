abbc45dd0c3686df63a7ca3073c0e8c5c507e6eb
Franco Arevalo <francoarevalo@csu.fullerton.edu>
2020-03-17 12:00:29 -0700

    Test #3

--- app/src/main/java/com/example/flexinfitness/logDiaryHomepage.java
+++ app/src/main/java/com/example/flexinfitness/logDiaryHomepage.java
@@ -16,6 +16,9 @@ public class logDiaryHomepage extends AppCompatActivity
         //Test
 
         //Test #2
+
+        //Test #3
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_diary_homepage);
     }
