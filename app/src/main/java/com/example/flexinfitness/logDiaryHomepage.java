6e8520c7814cdbffa60db0eb37a3e6e1bd061d6e
Franco Arevalo <francoarevalo@csu.fullerton.edu>
2020-03-17 12:12:06 -0700

    Erased the tests in the logDiaryHomepage activity

--- app/src/main/java/com/example/flexinfitness/logDiaryHomepage.java
+++ app/src/main/java/com/example/flexinfitness/logDiaryHomepage.java
@@ -10,15 +10,6 @@ public class logDiaryHomepage extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
-
-
-
-        //Test
-
-        //Test #2
-
-        //Test #3
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_diary_homepage);
     }
