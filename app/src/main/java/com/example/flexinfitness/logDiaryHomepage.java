8420669629d04ae0fcdf656e3efe4a18f71c30c8
Franco Arevalo <francoarevalo@csu.fullerton.edu>
2020-03-17 11:31:07 -0700

    Added the empty activity: logDiaryHomepage

---
+++ app/src/main/java/com/example/flexinfitness/logDiaryHomepage.java
@@ -0,0 +1,16 @@
+package com.example.flexinfitness;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class logDiaryHomepage extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_diary_homepage);
+    }
+}
