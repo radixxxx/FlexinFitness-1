26fd08355a9dde814728b527c0823f6dac715378
Franco Arevalo <francoarevalo@csu.fullerton.edu>
2020-03-22 09:06:27 -0700

    LogEntry does nothing when pressed

--- app/src/main/java/com/example/flexinfitness/logHomepage.java
+++ app/src/main/java/com/example/flexinfitness/logHomepage.java
@@ -2,12 +2,10 @@ package com.example.flexinfitness;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.constraintlayout.widget.ConstraintSet;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.LinearLayout;
 
 //start logHomepage class =====================================================================
@@ -47,10 +45,11 @@ public class logHomepage extends AppCompatActivity implements View.OnClickListen
                 // Create log entry
                 Button createdLogEntry = createLogEntry();
                 // Set a click listener to it
-                createdLogEntry.setOnClickListener(getOnClickDoSomething(createdLogEntry));
+                createdLogEntry.setOnClickListener(onEntryClick(createdLogEntry));
                 break;
         } // end switch(view.getId()
     } // end onClick() =============================================================================
+
 
     // start createLogEntry() ======================================================================
     public Button createLogEntry()
@@ -67,38 +66,10 @@ public class logHomepage extends AppCompatActivity implements View.OnClickListen
     } // end createLogEntry() ======================================================================
 
     // start getOnClickDoSomething() ===============================================================
-    View.OnClickListener getOnClickDoSomething(final Button button)
-    {
-        return new View.OnClickListener()
-        {
-            // there will be an edittext that appears when a log entry is pressed
-            public void onClick(View view)
-            {
-                // declare edit text
-                EditText logEntryName = new EditText(logHomepage.this);
+    View.OnClickListener onEntryClick(final Button button) {
+        return new View.OnClickListener() {
+            public void onClick(View view) {
 
-                // set properties of edit text
-                int ID = 0;
-                logEntryName.setId(ID);
-                logEntryName.setHint("ENTER WORKOUT NAME");
-                ConstraintLayout.LayoutParams clp_logEntryName = new ConstraintLayout.LayoutParams
-                        (ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT);
-                logEntryName.setLayoutParams(clp_logEntryName);
-
-                // add 'logEntryName' EditText View to c_constraintLayout
-                // logEntryName is a child of c_constraintLayout
-                constraintLayout.addView(logEntryName);
-
-                // setting constraints on 'logEntryName' EditText View
-                ConstraintSet constraintSet = new ConstraintSet();
-                constraintSet.clone(constraintLayout);
-
-                constraintSet.connect(logEntryName.getId(), ConstraintSet.LEFT, constraintLayout.getId(), ConstraintSet.LEFT);
-                constraintSet.connect(logEntryName.getId(), ConstraintSet.RIGHT, constraintLayout.getId(), ConstraintSet.RIGHT);
-                constraintSet.connect(logEntryName.getId(), ConstraintSet.TOP, constraintLayout.getId(), ConstraintSet.TOP);
-                constraintSet.connect(logEntryName.getId(), ConstraintSet.BOTTOM, constraintLayout.getId(), ConstraintSet.BOTTOM);
-
-                constraintSet.applyTo(constraintLayout);
             }
         };
     } // end getOnClickDoSomething() ===============================================================
