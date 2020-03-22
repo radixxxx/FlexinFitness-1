8a51296ce6fd24ffa60818d282e41b2b80a69d8b
Franco Arevalo <francoarevalo@csu.fullerton.edu>
2020-03-22 11:30:18 -0700

    declared View & ViewGroups and connected them to their XML's. Set the onClick's for the TextViews.

--- app/src/main/java/com/example/flexinfitness/createLogEntry.java
+++ app/src/main/java/com/example/flexinfitness/createLogEntry.java
@@ -6,19 +6,58 @@ import androidx.constraintlayout.widget.ConstraintSet;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class createLogEntry extends AppCompatActivity
+public class createLogEntry extends AppCompatActivity implements View.OnClickListener
 {
     // declare View & ViewGroup variables
+    EditText workoutNameEditText;
+    EditText dateEditText;
+    EditText startTimeEditText;
+    EditText durationEditText;
+    EditText bodyWeightEditText;
 
+    TextView rightWorkoutNameTextView;
+    TextView rightDateTextView;
+    TextView rightStartTimeTextView;
+    TextView rightDurationTextView;
+    TextView rightBodyWeightTextView;
 
+    // start onCreate() ============================================================================
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_log_entry);
 
+        // connect View & ViewGroups to their XML's
+        workoutNameEditText = findViewById(R.id.workoutNameEditText);
+        dateEditText = findViewById(R.id.dateEditText);
+        startTimeEditText = findViewById(R.id.startTimeEditText);
+        durationEditText = findViewById(R.id.durationEditText);
+        bodyWeightEditText = findViewById(R.id.bodyWeightEditText);
 
-    }
+        rightWorkoutNameTextView = findViewById(R.id.rightWorkoutNameTextView);
+        rightDateTextView = findViewById(R.id.rightDateTextView);
+        rightStartTimeTextView = findViewById(R.id.rightStartTimeTextView);
+        rightDurationTextView = findViewById(R.id.rightDurationTextView);
+        rightBodyWeightTextView = findViewById(R.id.rightBodyWeightTextView);
+
+        // set the 'TextViews' onClick's
+        rightWorkoutNameTextView.setOnClickListener(this);
+        rightDateTextView.setOnClickListener(this);
+        rightStartTimeTextView.setOnClickListener(this);
+        rightDurationTextView.setOnClickListener(this);
+        rightBodyWeightTextView.setOnClickListener(this);
+
+    } // end onCreate() ============================================================================
+
+    // start onClick() =============================================================================
+    @Override
+    public void onClick(View v)
+    {
+
+    } // end onClick() =============================================================================
 }
