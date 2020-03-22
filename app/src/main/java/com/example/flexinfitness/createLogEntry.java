cd4afba0ec0e418896cc774badb4af02db09ebca
Franco Arevalo <francoarevalo@csu.fullerton.edu>
2020-03-22 10:45:37 -0700

    semi done with the layout for 'createLogEntry'

--- app/src/main/java/com/example/flexinfitness/createLogEntry.java
+++ app/src/main/java/com/example/flexinfitness/createLogEntry.java
@@ -2,13 +2,16 @@ package com.example.flexinfitness;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class createLogEntry extends AppCompatActivity
 {
     // declare View & ViewGroup variables
-    ConstraintLayout constraintLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -16,7 +19,6 @@ public class createLogEntry extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_log_entry);
 
-        // connect View & ViewGroup variables
-        constraintLayout =
+
     }
 }
