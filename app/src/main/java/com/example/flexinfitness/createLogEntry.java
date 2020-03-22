aee1e94afd055150f99d7e8067f29159a6c9a9e3
Franco Arevalo <francoarevalo@csu.fullerton.edu>
2020-03-22 09:17:33 -0700

    declared view & viewgroup variables

--- app/src/main/java/com/example/flexinfitness/createLogEntry.java
+++ app/src/main/java/com/example/flexinfitness/createLogEntry.java
@@ -1,16 +1,22 @@
 package com.example.flexinfitness;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
 
 public class createLogEntry extends AppCompatActivity
 {
+    // declare View & ViewGroup variables
+    ConstraintLayout constraintLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_log_entry);
+
+        // connect View & ViewGroup variables
+        constraintLayout =
     }
 }
