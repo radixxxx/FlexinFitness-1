c8377f095de13856f821b2fd7538c991bc2f6f8b
Franco Arevalo <francoarevalo@csu.fullerton.edu>
2020-03-22 01:37:49 -0700

    Each programmatically created button now has an assigned onClick. It just changes the text to 'FUCK DOES THIS WORK'

--- app/src/main/java/com/example/flexinfitness/logDiaryHomepage.java
+++ app/src/main/java/com/example/flexinfitness/logDiaryHomepage.java
@@ -18,7 +18,6 @@ public class logDiaryHomepage extends AppCompatActivity implements View.OnClickL
     // for the entry)
     static int ID = 0;
 
-
     // start onCreate() ============================================================================
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -29,12 +28,9 @@ public class logDiaryHomepage extends AppCompatActivity implements View.OnClickL
         // Connect View & ViewGroup variables to their XML id's
         l_linearLayout = (LinearLayout) findViewById(R.id.linearLayout);
         addLogButton = (Button) findViewById(R.id.buttonADDLOG);
-
-
         addLogButton.setOnClickListener(this);
 
     } // end onCreate() ============================================================================
-
 
     // start onClick() =============================================================================
     @Override
@@ -45,14 +41,38 @@ public class logDiaryHomepage extends AppCompatActivity implements View.OnClickL
             // Adds a new entry to the scroll view
             case R.id.buttonADDLOG:
                 // Create log entry
-                ID += 1;
-                String str_ID = Integer.toString(ID);
-                Button createdLogEntry = new Button(this);
-                createdLogEntry.setText(("Entry " + str_ID));
-                l_linearLayout.addView(createdLogEntry);
+                Button createdLogEntry = createLogEntry();
+                // Set a click listener to it
+                createdLogEntry.setOnClickListener(getOnClickDoSomething(createdLogEntry));
                 break;
         } // end switch(view.getId()
 
     } // end onClick() =============================================================================
+
+    // start createLogEntry() ======================================================================
+    public Button createLogEntry()
+    {
+        ID += 1;
+        int firstEntry = ID;
+        String str_ID = Integer.toString(ID);
+        Button createdLogEntry = new Button(this);
+        createdLogEntry.setText(("Entry " + str_ID));
+        createdLogEntry.setId(firstEntry);
+        l_linearLayout.addView(createdLogEntry);
+        return createdLogEntry;
+    } // end createLogEntry() ======================================================================
+
+    // start getOnClickDoSomething() ===============================================================
+    View.OnClickListener getOnClickDoSomething(final Button button)
+    {
+        return new View.OnClickListener()
+        {
+            public void onClick(View view)
+            {
+                button.setText("FUCK DOES THIS WORK");
+            }
+        };
+    } // end getOnClickDoSomething() ===============================================================
+
 
 } // end logDiaryHomepage class ====================================================================
