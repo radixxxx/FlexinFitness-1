88681f75d9f529f3f01a35e5f545d9a11ec9ab05
Franco Arevalo <francoarevalo@csu.fullerton.edu>
2020-03-24 14:17:49 -0700

    added workoutPlanner activity

---
+++ app/src/main/java/com/example/flexinfitness/workoutPlanner.java
@@ -0,0 +1,16 @@
+package com.example.flexinfitness;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class workoutPlanner extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workout_planner);
+    }
+}
