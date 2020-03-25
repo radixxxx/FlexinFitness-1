b42f7b80073aa641ef4284a11a0bda86be65c16b
Franco Arevalo <francoarevalo@csu.fullerton.edu>
2020-03-24 19:35:26 -0700

    declared Views&ViewGroups | connected them to their XML's | declared onClick() it's empty though

--- app/src/main/java/com/example/flexinfitness/workoutPlanner.java
+++ app/src/main/java/com/example/flexinfitness/workoutPlanner.java
@@ -3,14 +3,52 @@ package com.example.flexinfitness;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
-public class workoutPlanner extends AppCompatActivity
+// start workoutPlanner class ======================================================================
+public class workoutPlanner extends AppCompatActivity implements View.OnClickListener
 {
+    // declare View & ViewGroups
+    Button doneButton;
+    Button submitButton;
 
+    EditText workoutNameEditText;
+    EditText dateToPerformEditText;
+    EditText timeToPerformEditText;
+    EditText restBetweenSetsEditText;
+
+    LinearLayout linearLayout;
+
+    // start onCreate() ============================================================================
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout_planner);
-    }
-}
+
+        // connect Views & ViewGroups to XMLs
+        doneButton = findViewById(R.id.doneButton);
+        submitButton = findViewById(R.id.submitButton);
+
+        workoutNameEditText = findViewById(R.id.workoutNameEditText);
+        dateToPerformEditText = findViewById(R.id.dateToPerformEditText);
+        timeToPerformEditText = findViewById(R.id.timeToPerformEditText);
+        restBetweenSetsEditText = findViewById(R.id.restBetweenSetsEditText);
+
+        linearLayout = findViewById(R.id.linearLayoutInsideScrollView);
+    } // end onCreate() ============================================================================
+
+    // start onClick() =============================================================================
+    @Override
+    public void onClick(View v)
+    {
+        switch (v.getId())
+        {
+            // case R.id.<nameOfViewID>
+        }
+
+    } // end onClick() =============================================================================
+} // end workoutPlanner class ======================================================================
